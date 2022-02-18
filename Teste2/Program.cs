2c75906b364a617cdfbf8b52fa898205d9da1502
Lucas <Lucas@DESKTOP-JUJ0AHE>
2022-02-18 12:32:51 -0300

    Add project files.

---
+++ Teste2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Teste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            char [] letras = alfabeto.ToCharArray();
+            char letrasDiamante;
+            Console.WriteLine("Digite alguma letra do alfabeto");
+            letrasDiamante = Convert.ToChar(Console.ReadLine());
+            // contadores
+            int contadorletras = 0, contadorespaco = 0;
+
+            #region contador
+            for (int i = 0; i <= letras.Length; i++)
+            {
+                if (letrasDiamante == letras[i])
+                {
+                    contadorletras++;
+                    break;
+                }
+                else contadorletras++;
+            }
+            #endregion
+
+            #region espaços da parte de cima:
+
+            for (int linha = 0; contadorletras > 0; linha++)
+            {
+                //espaços
+                for (int i = 0; i < contadorletras - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                //letras
+                Console.Write(letras[linha]);
+
+                //espaço dentro
+                for (int i = 1; i < contadorespaco; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                //p/ nao mostrar a letra duas vezes na 1 linha
+                if (linha != 0)
+                    Console.Write(letras[linha]);
+
+                contadorletras--;
+                contadorespaco += 2;
+                Console.WriteLine();
+                Console.WriteLine();
+                #endregion
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testes.01/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Diamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                #region variaveis
+                string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                char[] letras = alfabeto.ToCharArray();
+                int contadorLetras = 0, espacoInterno = 0;
+                // declaração local
+                char letrasDiamante;
+                #endregion
+                Console.WriteLine("Insira uma letra do alfabeto, só funciona com (E) kkkkkkkkkkkk)");
+                letrasDiamante = Convert.ToChar(Console.ReadLine());
+
+
+                #region contador
+                for (int i = 0; i <= letras.Length; i++)
+                {
+                    if (letrasDiamante == letras[i])
+                    {
+                        contadorLetras++;
+                        break;
+                    }
+                    else contadorLetras++;
+                }
+                #endregion
+
+                #region espaços da parte de cima:
+
+                for (int linha = 0; contadorLetras > 0; linha++)
+                {
+                    //espaços
+                    for (int i = 0; i < contadorLetras - 1; i++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    //letras
+                    Console.Write(letras[linha]);
+
+                    //espaço dentro
+                    for (int i = 1; i < espacoInterno; i++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    //p/ nao mostrar a letra duas vezes na 1 linha
+                    if (linha != 0)
+                        Console.Write(letras[linha]);
+
+                    contadorLetras--;
+                    espacoInterno += 2;
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    #endregion
+                }
+                int letra = 3;
+                contadorLetras = 1;
+                espacoInterno = 7;
+
+                for (int linha = 1; espacoInterno > 0; linha++)
+                {
+                    for (int i = 1; i <= contadorLetras; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(letras[letra]);
+                    //espaço de dentro
+                    for (int i = 1; i <= espacoInterno - 2; i++)    
+                    {
+                        Console.Write(" ");
+                    }
+                //mostras a letra com a condição de nao repetir
+                if(linha != 4)
+                        Console.Write(letras[letra]);
+
+                    letra--;
+                    contadorLetras++;
+                    espacoInterno -= 2;
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    
+                        
+                }
+                Console.ReadKey();
+
+
+            }
+
+        }
+
+    }
+         
+           
+    }
+    
+
+
